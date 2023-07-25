ca65eaf9aca69b49c6f64de9dc56eff72a26be64
sumanth <sumanths947@gmail.com>
2023-07-25 14:45:21 +0530

    Added Create Funtion to Phonebook

--- phonebook.c
+++ phonebook.c
@@ -1,29 +1,56 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-/*  struct PhoneBookEntry{
-   char name[25];
-   char address[50];
-   char phone_number[12];
-   }
-   
-   strcut listnode{
-    struct PhoneBookEntry data;
-    struct listnode *next;
-    }
-    
-    */
-    
-    void clear_screen(){
-    //this is an ANSI escape sequence to clear the terminal
-    printf("\033[2J\033[H");
-    }
-    
-    
+
+#define MAX_LENGTH_NAME 25
+#define MAX_LENGTH_ADDRESS 50
+#define MAX_LENGTH_NUMBER 12
+#define MAX_ENTRIES 30
+
+
+struct PhonebookEntry
+ {
+   char name[MAX_LENGTH_NAME];
+   char address[MAX_LENGTH_ADDRESS];
+   char phoneNumber[MAX_LENGTH_NUMBER];
+ };
+
+
+ 
+	 void createEntry(struct PhonebookEntry *phonebook,int *numEntries)//to create new entries
+	 {
+	  //check if array is full
+	    if(*numEntries >= MAX_ENTRIES)
+	      {
+	       printf("Phonebook is full,cannot add new entry\n");
+	       return;
+	       }
+	     struct PhonebookEntry newEntry;
+	     printf("\n\n\t\u2022Enter the phone number (Max Number %d:)",MAX_LENGTH_NUMBER-1);
+	     fgets(newEntry.phoneNumber,sizeof(newEntry.phoneNumber),stdin);
+	     newEntry.phoneNumber[strcspn(newEntry.phoneNumber,"\n")]='\0';
+	     
+	     
+	     printf("\n\n\t\u2022Enter the name (Max Character %d:)",MAX_LENGTH_NAME-1);
+	     fgets(newEntry.name,sizeof(newEntry.name),stdin);
+	     newEntry.name[strcspn(newEntry.name,"\n")]='\0';
+	     
+	     
+	     printf("\n\n\t\u2022Enter the Address (Max Address Character %d:)",MAX_LENGTH_ADDRESS-1);
+	     fgets(newEntry.address,sizeof(newEntry.address),stdin);
+	     newEntry.address[strcspn(newEntry.address,"\n")]='\0';
+	     
+	     
+	     phonebook[*numEntries] = newEntry;
+	     
+	     (*numEntries)++;
+	     printf("new entry added to phonebook");
+	     
+	 }   
     int display_menu()
      { 
      //Main MENU
-     clear_screen();
+     system("clear");
        printf("\n\n===============================================================================================PHONE BOOK==================================================================================================\n");
        
        printf("\n\t1.Create new entry\n");
@@ -37,20 +64,23 @@
     
     int main()
     {
+    
+    struct PhonebookEntry phonebook[MAX_ENTRIES];
+    int numEntries=0;
      char choice;
      //yet to write the linked list code :
      
      while(1){
      display_menu();
-     scanf("%c",&choice);
-     
+     scanf(" %c",&choice);
+     getchar();
      switch(choice){
          case '1':
-                   //call create funtion
+                   createEntry(phonebook,&numEntries);
                    printf("You Selected 'Create new entry'.\n");
                    break;
           case '2':
-                   //call view entry funtion
+                   viewEntry(phonebook,&numEntries);
                    printf("You Selected 'View  Entries.\n");
                    break;
            case '3':
